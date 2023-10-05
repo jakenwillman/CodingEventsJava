58fff093b317497f77b8dbbf21880f2f743504ce
Jake Willman <jakenwillman@gmail.com>
2023-10-04 22:49:32 -0500

    added functionality to delete events

--- Spring Boot/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ Spring Boot/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -23,17 +23,35 @@ public class EventController {
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    // lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+    public String processCreateEventForm(@RequestParam String eventName,
+                                         @RequestParam String eventDescription) {
         EventData.add(new Event(eventName, eventDescription));
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
         return "redirect:/events";
     }
 
